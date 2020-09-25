19caea06d95a870d207fed057090adf56625285c
Miles Hammock <miles0569@gmail.com>
2020-09-25 10:45:59 -0700

    Test

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -52872,7 +52872,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60920" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59094" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
